3095e8dfee8c7104cb6f2385ba4c46d753911eb7
DELL-XPS-15\salaz <salazar3antonio@gmail.com>
2022-08-26 17:49:57 -0500

    new date

--- src/main/java/dates/DateExample.java
+++ src/main/java/dates/DateExample.java
@@ -1,5 +1,6 @@
 package dates;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -7,7 +8,7 @@ import java.util.Locale;
 
 public class DateExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(2082, Calendar.JANUARY, 31);
@@ -19,6 +20,18 @@ public class DateExample {
         String hoursAndMinsFormatted = hoursAndMins.format(date);
         System.out.println(hoursAndMinsFormatted);
 
+        String dateAsString = "01/08/2022";
+        Date olDate = new SimpleDateFormat("MM/dd/yyyy").parse(dateAsString);
+
+        System.out.println("New Date " + addDaysToDate(olDate, 4));
+
+    }
+
+    private static Date addDaysToDate(Date oldDate, int daysToAdd) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(oldDate);
+        c.add(Calendar.DAY_OF_MONTH, daysToAdd);
+        return c.getTime();
     }
 
 
